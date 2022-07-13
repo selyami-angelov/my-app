89c773ec7af96cd69e814f1a0950d8b78825c9c3
selyami-angelov <74262500+selyami-angelov@users.noreply.github.com>
2022-07-13 18:48:21 +0300

    refactor: rename env variables

--- src/configs/firebase-config.js
+++ src/configs/firebase-config.js
@@ -3,16 +3,14 @@ import { getFirestore } from 'firebase/firestore'
 import { getStorage } from 'firebase/storage'
 
 const firebaseConfig = {
-  apiKey: process.env.XLO_FIREBASE_API_KEY,
-  authDomain: process.env.XLO_AUTH_DOMAIN,
-  projectId: process.env.XLO_PROJECT_ID,
-  storageBucket: process.env.XLO_STORAGE_BUCKET,
-  messagingSenderId: process.env.XLO_MESSAGING_SENDER_ID,
-  appId: process.env.XLO_APP_ID,
-  measurementId: process.env.XLO_MEASUREMENT_ID,
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.AUTH_DOMAIN,
+  projectId: process.env.PROJECT_ID,
+  storageBucket: process.env.STORAGE_BUCKET,
+  messagingSenderId: process.env.MESSAGING_SENDER_ID,
+  appId: process.env.APP_ID,
+  measurementId: process.env.MEASUREMENT_ID,
 }
-
-XLO_MEASUREMENT_ID = G - NGTFVTE8Z6
 
 const app = initializeApp(firebaseConfig)
 export const db = getFirestore(app)
