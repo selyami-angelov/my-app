e64466e05de6e7c34dbfbd150b694a76a8c10678
selyami-angelov <74262500+selyami-angelov@users.noreply.github.com>
2022-08-01 20:59:23 +0300

    feature: create user doc on first login

--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -1,4 +1,5 @@
 import { createContext, useEffect, useReducer } from 'react'
+import { getUserDoc, createUserDoc } from '../services/userData.js'
 import AuthReducer from './AuthReducer'
 
 const INITIAL_STATE = {
@@ -12,6 +13,28 @@ export const AuthContextProvider = ({ children }) => {
 
   useEffect(() => {
     localStorage.setItem('user', JSON.stringify(state.currentUser))
+  }, [state.currentUser])
+
+  //create user doc if first login
+  useEffect(() => {
+    if (state.currentUser) {
+      getUserDoc(state.currentUser?.uid).then((result) => {
+        if (result === 'No such document!') {
+          const userData = {
+            userId: state.currentUser.uid,
+            name: state.currentUser.displayName || '',
+            email: state.currentUser.email,
+            createdAt: state.currentUser.metadata.createdAt,
+            lastLoginAt: state.currentUser.metadata.lastLoginAt,
+            followed: [],
+          }
+
+          console.log(userData, 'result in context')
+          createUserDoc(state.currentUser.uid, userData)
+        }
+      })
+    }
+    console.log(state.currentUser, 'user from context')
   }, [state.currentUser])
 
   return (
