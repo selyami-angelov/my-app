2a329bb8081dfd4ae11d5235cad6774573d24f30
selyami-angelov <74262500+selyami-angelov@users.noreply.github.com>
2022-08-12 22:01:45 +0300

    fix: clean products state on useEffect  trigger

--- src/pages/followed-products/FollowedProducts.js
+++ src/pages/followed-products/FollowedProducts.js
@@ -4,13 +4,17 @@ import CardContainer from '../../components/CardContainer/CardContainer.js'
 import ProductCard from '../../components/ProductCard/ProductCard.js'
 import SearchToolbar from '../../components/SearchToolbar/SearchToolbar.js'
 import useFollowedProducts from '../../hook/useFollowedProducts.js'
-import { getAd, getProductDoc } from '../../services/product.js'
+import { getProductDoc } from '../../services/product.js'
+import styles from './FollowedProducts.module.css'
 
 const FollowedProducts = () => {
   const [products, setProducts] = useState([])
   const followed = useFollowedProducts()
 
+  console.log(products)
+
   useEffect(() => {
+    setProducts([])
     followed.forEach((product) => {
       getProductDoc(product).then((result) => {
         setProducts((prev) => [...prev, result])
@@ -18,33 +22,37 @@ const FollowedProducts = () => {
     })
   }, [followed])
 
+  console.log(products.length)
+
   return (
     <>
       <SearchToolbar />
-      <CardContainer
-        cards={
-          <Row className="g-4">
-            {products.length ? (
-              products.map((doc) => (
-                <Col key={doc.id} xs lg="3">
-                  <ProductCard
-                    id={doc.id}
-                    title={doc.data.title}
-                    img={doc.data.images[0]?.url}
-                    city={doc.data.city}
-                    created_date={doc.data.created_date}
-                    price={doc.data.price}
-                    currency={doc.data.currency}
-                  />
-                </Col>
-              ))
-            ) : (
-              <></>
-            )}
-          </Row>
-        }
-        title="Любими обяви"
-      ></CardContainer>
+      <div className={styles['body']}>
+        <CardContainer
+          cards={
+            <Row className="g-4">
+              {products.length ? (
+                products.map((doc) => (
+                  <Col key={doc.id} xs lg="3">
+                    <ProductCard
+                      id={doc.id}
+                      title={doc.data.title}
+                      img={doc.data.images[0]?.url}
+                      city={doc.data.city}
+                      created_date={doc.data.created_date}
+                      price={doc.data.price}
+                      currency={doc.data.currency}
+                    />
+                  </Col>
+                ))
+              ) : (
+                <></>
+              )}
+            </Row>
+          }
+          title="Любими обяви"
+        ></CardContainer>
+      </div>
     </>
   )
 }
